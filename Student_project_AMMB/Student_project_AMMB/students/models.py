a87276ebf87d7967fb79ba42556cfe2e42a7f745
ValentinPashev <javaddmaster96@gmail.com>
2024-10-01 18:07:37 +0300

    Add files via upload

--- Student_project_AMMB/Student_project_AMMB/students/models.py
+++ Student_project_AMMB/Student_project_AMMB/students/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+
+class Student(models.Model):
+
+    name = models.CharField(
+        max_length=100
+    )
+
+    age = models.IntegerField(
+
+    )
+
+    faculty_number = models.IntegerField(
+
+    )
+
+    date_of_birth = models.DateField(
+
+        blank=True,
+        null=True
+    )
+
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+        null=True,
+        blank=True,
+        editable=False,
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.faculty_number}")
+
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return f"{self.name}-{self.faculty_number}"
